79150a9b4113751fe1070f2ed270c1b8441574c0
Sai Kranthi <iamsaikranthi@gmail.com>
2023-04-01 13:42:58 +0530

    feat: add text and font-family to selected two tiles

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,47 @@
 use bevy::prelude::*;
 use itertools::Itertools;
+use rand::prelude::*;
 
 const TILE_SIZE: f32 = 40.0;
 const TILE_SPACER: f32 = 10.0;
 
+struct FontSpec {
+    family: Handle<Font>,
+}
+
+impl FromWorld for FontSpec {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.get_resource_mut::<AssetServer>().unwrap();
+        FontSpec {
+            family: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        }
+    }
+}
+
 struct Board {
     size: u8,
+    physical_size: f32,
+}
+
+impl Board {
+    fn new(size: u8) -> Self {
+        let physical_size = f32::from(size) * TILE_SIZE + f32::from(size + 1) * TILE_SPACER;
+        Board {
+            size,
+            physical_size,
+        }
+    }
+    fn cell_position_to_physical(&self, pos: u8) -> f32 {
+        let offset = -self.physical_size / 2.0 + 0.5 * TILE_SIZE;
+
+        offset + f32::from(pos) * TILE_SIZE + f32::from(pos + 1) * TILE_SPACER
+    }
 }
 
 struct Materials {
     board: Handle<ColorMaterial>,
     tile_placeholder: Handle<ColorMaterial>,
+    tile: Handle<ColorMaterial>,
 }
 
 impl FromWorld for Materials {
@@ -19,16 +50,30 @@ impl FromWorld for Materials {
         Materials {
             board: materials.add(Color::rgb(0.7, 0.7, 0.8).into()),
             tile_placeholder: materials.add(Color::rgb(0.75, 0.75, 0.9).into()),
+            tile: materials.add(Color::rgb(0.9, 0.9, 1.0).into()),
         }
     }
 }
 
+struct Points {
+    value: u32,
+}
+
+struct Position {
+    x: u8,
+    y: u8,
+}
+
+struct TileText;
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .init_resource::<FontSpec>()
         .init_resource::<Materials>()
         .add_startup_system(setup.system())
         .add_startup_system(spawn_board.system())
+        .add_startup_system_to_stage(StartupStage::PostStartup, spawn_tiles.system())
         .run();
 }
 
@@ -37,14 +82,14 @@ fn setup(mut commands: Commands) {
 }
 
 fn spawn_board(mut commands: Commands, materials: Res<Materials>) {
-    let board = Board { size: 4 };
+    let board = Board::new(4);
     let physical_board_size =
         f32::from(board.size) * TILE_SIZE + f32::from(board.size + 1) * TILE_SPACER;
 
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.board.clone(),
-            sprite: Sprite::new(Vec2::new(physical_board_size, physical_board_size)),
+            sprite: Sprite::new(Vec2::new(board.physical_size, board.physical_size)),
             ..Default::default()
         })
         .with_children(|builder| {
@@ -68,4 +113,55 @@ fn spawn_board(mut commands: Commands, materials: Res<Materials>) {
             }
         })
         .insert(board);
+}
+
+fn spawn_tiles(
+    mut commands: Commands,
+    materials: Res<Materials>,
+    query_board: Query<&Board>,
+    font_spec: Res<FontSpec>,
+) {
+    let board = query_board.single().expect("always expect a board");
+
+    let mut rng = rand::thread_rng();
+    let starting_tiles: Vec<(u8, u8)> = (0..board.size)
+        .cartesian_product(0..board.size)
+        .choose_multiple(&mut rng, 2);
+    for (x, y) in starting_tiles.iter() {
+        let pos = Position { x: *x, y: *y };
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.tile.clone(),
+                sprite: Sprite::new(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                transform: Transform::from_xyz(
+                    board.cell_position_to_physical(pos.x),
+                    board.cell_position_to_physical(pos.y),
+                    1.0,
+                ),
+                ..Default::default()
+            })
+            .with_children(|child_builder| {
+                child_builder
+                    .spawn_bundle(Text2dBundle {
+                        text: Text::with_section(
+                            "2",
+                            TextStyle {
+                                font: font_spec.family.clone(),
+                                font_size: 40.0,
+                                color: Color::BLACK,
+                                ..Default::default()
+                            },
+                            TextAlignment {
+                                vertical: VerticalAlign::Center,
+                                horizontal: HorizontalAlign::Center,
+                            },
+                        ),
+                        transform: Transform::from_xyz(0.0, 0.0, 1.0),
+                        ..Default::default()
+                    })
+                    .insert(TileText);
+            })
+            .insert(Points { value: 2 })
+            .insert(pos);
+    }
 }
