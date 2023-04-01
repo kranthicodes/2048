2ea4e6d3c9968f02701e7912d91a34453e4cc57c
Sai Kranthi <iamsaikranthi@gmail.com>
2023-04-01 12:55:34 +0530

    feat: add FromWorld trait to Materials struct

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,23 @@ struct Board {
     size: u8,
 }
 
+struct Materials {
+    board: Handle<ColorMaterial>,
+}
+
+impl FromWorld for Materials {
+    fn from_world(world: &mut World) -> Self {
+        let mut materials = world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
+        Materials {
+            board: materials.add(Color::rgb(0.7, 0.7, 0.8).into()),
+        }
+    }
+}
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .init_resource::<Materials>()
         .add_startup_system(setup.system())
         .add_startup_system(spawn_board.system())
         .run();
